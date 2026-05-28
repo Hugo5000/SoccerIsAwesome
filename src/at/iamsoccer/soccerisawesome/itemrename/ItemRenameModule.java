0d48b0da340a6884bf032f94840afb6d6fd4e0c4
Hugo B <bayer.hugo@aon.at>
2026-05-28 22:39:54 +0200

    added basic abstraction

--- src/at/iamsoccer/soccerisawesome/itemrename/ItemRenameModule.java
+++ src/at/iamsoccer/soccerisawesome/itemrename/ItemRenameModule.java
@@ -1,26 +1,22 @@
 package at.iamsoccer.soccerisawesome.itemrename;
 
-import at.hugob.plugin.library.config.MiniMsgLegacyHybridSerializer;
 import at.iamsoccer.soccerisawesome.AbstractModule;
-import at.iamsoccer.soccerisawesome.DecorationResolvers;
 import at.iamsoccer.soccerisawesome.SoccerIsAwesomePlugin;
 import at.iamsoccer.soccerisawesome.itemrename.dialog.ItemCustomNameRenameDialog;
 import at.iamsoccer.soccerisawesome.itemrename.dialog.ItemLoreRenameDialog;
+import at.iamsoccer.soccerisawesome.itemrename.dialog.ItemNameRenameDialog;
 import com.mojang.brigadier.Command;
 import io.papermc.paper.command.brigadier.Commands;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.Style;
-import net.kyori.adventure.text.format.TextDecoration;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
-import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class ItemRenameModule extends AbstractModule implements Listener {
+    private final ItemCustomNameRenameDialog itemCustomNameRenameDialog = new ItemCustomNameRenameDialog();
+    private final ItemNameRenameDialog itemNameRenameDialog = new ItemNameRenameDialog();
+    private final ItemLoreRenameDialog itemLoreRenameDialog = new ItemLoreRenameDialog();
+
     public ItemRenameModule(SoccerIsAwesomePlugin plugin) {
         super(plugin, "ItemRename");
     }
@@ -31,7 +27,7 @@ public class ItemRenameModule extends AbstractModule implements Listener {
                 .requires(css -> css.getSender() instanceof Player player && player.hasPermission("shia.rename.custom-name.command"))
                 .executes(ctx -> {
                     Player player = (Player) ctx.getSource().getSender();
-                    player.showDialog(ItemCustomNameRenameDialog.create(player));
+                    player.showDialog(itemCustomNameRenameDialog.create(player));
                     return Command.SINGLE_SUCCESS;
                 })
                 .build(),
@@ -42,67 +38,23 @@ public class ItemRenameModule extends AbstractModule implements Listener {
                 .requires(css -> css.getSender() instanceof Player player && player.hasPermission("shia.rename.lore"))
                 .executes(ctx -> {
                     Player player = (Player) ctx.getSource().getSender();
-                    player.showDialog(ItemLoreRenameDialog.create(player));
+                    player.showDialog(itemLoreRenameDialog.create(player));
                     return Command.SINGLE_SUCCESS;
                 })
                 .build(),
             "Allows changing an Items Lore",
             List.of("setlore")
         );
+        register.register(Commands.literal("reitemname")
+                .requires(css -> css.getSender() instanceof Player player && player.hasPermission("shia.rename.item-name"))
+                .executes(ctx -> {
+                    Player player = (Player) ctx.getSource().getSender();
+                    player.showDialog(itemLoreRenameDialog.create(player));
+                    return Command.SINGLE_SUCCESS;
+                })
+                .build(),
+            "Allows changing an Items Lore",
+            List.of("setitemname")
+        );
     }
-
-    public static final @NotNull Style COMPACT_STYLE = Style.style()
-        .color(NamedTextColor.WHITE)
-        .decoration(TextDecoration.ITALIC, false)
-        .decoration(TextDecoration.OBFUSCATED, false)
-        .decoration(TextDecoration.BOLD, false)
-        .decoration(TextDecoration.STRIKETHROUGH, false)
-        .decoration(TextDecoration.UNDERLINED, false)
-        .build();
-
-    public static @NotNull MiniMessage serializerFor(Player player) {
-        var builder = MiniMessage.builder();
-        var resolver = TagResolver.builder();
-
-        resolver.resolver(StandardTags.reset());
-        resolver.resolver(StandardTags.translatable());
-        if (player.hasPermission("shia.rename.format.color")) {
-            resolver.resolver(StandardTags.color());
-            resolver.resolver(StandardTags.gradient());
-            resolver.resolver(StandardTags.rainbow());
-            resolver.resolver(StandardTags.pride());
-            builder.preProcessor(MiniMsgLegacyHybridSerializer::parseLegacy);
-        }
-        if (player.hasPermission("shia.rename.format.font")) {
-            resolver.resolver(StandardTags.font());
-        }
-        if (player.hasPermission("shia.rename.format.shadow")) {
-            resolver.resolver(StandardTags.shadowColor());
-        }
-        if (player.hasPermission("shia.rename.format.sprites")) {
-            resolver.resolver(StandardTags.sprite());
-        }
-        if (player.hasPermission("shia.rename.format.keybind")) {
-            resolver.resolver(StandardTags.keybind());
-        }
-        if (player.hasPermission("shia.rename.format.italic")) {
-            resolver.resolver(DecorationResolvers.ITALIC);
-        }
-        if (player.hasPermission("shia.rename.format.bold")) {
-            resolver.resolver(DecorationResolvers.BOLD);
-        }
-        if (player.hasPermission("shia.rename.format.obfuscated")) {
-            resolver.resolver(DecorationResolvers.OBFUSCATED);
-        }
-        if (player.hasPermission("shia.rename.format.strikethrough")) {
-            resolver.resolver(DecorationResolvers.STRIKETHROUGH);
-        }
-        if (player.hasPermission("shia.rename.format.underlined")) {
-            resolver.resolver(DecorationResolvers.UNDERLINED);
-        }
-        builder.tags(resolver.build());
-        builder.postProcessor(c -> c.compact(COMPACT_STYLE));
-        return builder.build();
-    }
-
 }
--- src/at/iamsoccer/soccerisawesome/itemrename/dialog/ItemNameRenameDialog.java
+++ src/at/iamsoccer/soccerisawesome/itemrename/dialog/ItemNameRenameDialog.java
@@ -1,129 +1,43 @@
 package at.iamsoccer.soccerisawesome.itemrename.dialog;
 
-import at.hugob.plugin.library.config.MiniMsgLegacyHybridSerializer;
-import at.iamsoccer.soccerisawesome.itemrename.ItemRenameModule;
 import io.papermc.paper.datacomponent.DataComponentTypes;
-import io.papermc.paper.dialog.Dialog;
-import io.papermc.paper.dialog.DialogResponseView;
-import io.papermc.paper.registry.data.dialog.ActionButton;
-import io.papermc.paper.registry.data.dialog.DialogBase;
-import io.papermc.paper.registry.data.dialog.action.DialogAction;
-import io.papermc.paper.registry.data.dialog.body.DialogBody;
-import io.papermc.paper.registry.data.dialog.input.DialogInput;
-import io.papermc.paper.registry.data.dialog.input.TextDialogInput;
-import io.papermc.paper.registry.data.dialog.type.DialogType;
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.dialog.DialogLike;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickCallback;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
-import org.jspecify.annotations.NonNull;
 
-import java.time.temporal.ChronoUnit;
-import java.util.List;
-
-import static at.iamsoccer.soccerisawesome.itemrename.ItemRenameModule.COMPACT_STYLE;
-
-public class ItemNameRenameDialog {
-    public static final ClickCallback.Options UNLIMITED_CALLBACK_OPTIONS = ClickCallback.Options.builder().uses(-1).lifetime(ChronoUnit.FOREVER.getDuration()).build();
-    private static final NamespacedKey INPUT_DATA_KEY = NamespacedKey.fromString("rename:item_name");
-
-    private ItemNameRenameDialog() {
+public class ItemNameRenameDialog extends AbstractRenameDialog {
+    public ItemNameRenameDialog() {
+        super(NamespacedKey.fromString("rename:item_name"));
     }
 
-    private static final DialogAction applyAction = DialogAction.customClick(ItemNameRenameDialog::onApply, UNLIMITED_CALLBACK_OPTIONS);
-    private static final DialogAction previewAction = DialogAction.customClick(ItemNameRenameDialog::onPreview, UNLIMITED_CALLBACK_OPTIONS);
-
-    public static DialogLike create(Player player) {
-        var item = player.getInventory().getItemInMainHand();
-
-        final String text;
-        if (item.getPersistentDataContainer().has(INPUT_DATA_KEY, PersistentDataType.STRING)) {
-            text = item.getPersistentDataContainer().get(INPUT_DATA_KEY, PersistentDataType.STRING);
-        } else if (item.hasData(DataComponentTypes.CUSTOM_NAME)) {
-            text = parseComponent(item.getData(DataComponentTypes.CUSTOM_NAME));
+    @Override
+    protected String getSuggestionFromItem(ItemStack item) {
+        if (item.getPersistentDataContainer().has(pdcDataKey, PersistentDataType.STRING)) {
+            return item.getPersistentDataContainer().get(pdcDataKey, PersistentDataType.STRING);
         } else if (item.hasData(DataComponentTypes.ITEM_NAME)) {
-            text = parseComponent(item.getData(DataComponentTypes.ITEM_NAME));
+            return parseComponent(item.getData(DataComponentTypes.ITEM_NAME));
         } else {
-            text = parseComponent(item.effectiveName());
+            return parseComponent(item.effectiveName());
         }
-        // TODO: flatten the suggestion
-        return getDialog(player, item, text);
     }
 
-    private static @NonNull Dialog getDialog(Player player, ItemStack itemStack, String text) {
-        MiniMessage instance = MiniMsgLegacyHybridSerializer.INSTANCE;
-        var name = instance.deserialize(text);
-        var item = itemStack.clone();
-        applyLore(player, text, item);
-        return Dialog.create(builder ->
-            builder.empty().base(DialogBase.builder(Component.text("Item Rename"))
-                    .body(List.of(
-                        DialogBody.plainMessage(name),
-                        DialogBody.item(item).build()
-                    ))
-                    .inputs(List.of(
-                        DialogInput.text("name", Component.text("Item Name"))
-                            .maxLength(16000)
-                            .initial(text)
-                            .multiline(TextDialogInput.MultilineOptions.create(null, 150))
-                            .build()
-                    ))
-                    .canCloseWithEscape(true)
-                    .build())
-                .type(DialogType.multiAction(List.of(
-                    ActionButton.builder(Component.text("Preview Name")).action(previewAction).build(),
-                    ActionButton.builder(Component.text("Apply")).action(applyAction).build()
-                ), ActionButton.builder(Component.text("Cancel")).build(), 1))
-        );
-    }
-
-    private static @NonNull String parseComponent(Component component) {
-        return MiniMsgLegacyHybridSerializer.INSTANCE.serialize(component.compact(COMPACT_STYLE));
-    }
-
-    private static void onApply(DialogResponseView response, Audience audience) {
-        if (!(audience instanceof Player player)) return;
-        var item = player.getInventory().getItemInMainHand();
-        String input = response.getText("name");
-        // TODO: limit length
-        applyLore(player, input, item);
-        item.editPersistentDataContainer(pdc -> {
-            if (input.isBlank()) {
-                pdc.remove(INPUT_DATA_KEY);
-            } else {
-                pdc.set(INPUT_DATA_KEY, PersistentDataType.STRING, input);
-            }
-        });
-    }
-
-    private static void applyLore(Player player, String input, ItemStack item) {
+    @Override
+    protected void applyToItem(Player player, String input, ItemStack item) {
         if (input.isBlank()) {
             item.resetData(DataComponentTypes.ITEM_NAME);
         } else {
-            item.setData(DataComponentTypes.ITEM_NAME, parseInput(player, input));
+            item.setData(DataComponentTypes.ITEM_NAME, parseLine(player, input));
         }
     }
 
-    private static @NonNull Component parseInput(Player player, String input) {
-        return ItemRenameModule.serializerFor(player).deserialize(input)
-            .decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE)
-            .colorIfAbsent(NamedTextColor.WHITE);
+    @Override
+    protected void applyToPDC(PersistentDataContainer pdc, String input) {
+        if (input.isBlank()) {
+            pdc.remove(pdcDataKey);
+        } else {
+            pdc.set(pdcDataKey, PersistentDataType.STRING, input);
+        }
     }
-
-    private static void onPreview(DialogResponseView response, Audience audience) {
-        if (!(audience instanceof Player player)) return;
-        audience.showDialog(getDialog(player, player.getInventory().getItemInMainHand(), response.getText("name")));
-    }
-
-    // <bold><#392216>C<#3E2719>h<#432B1C>o<#48301F>c<#4C3422>o<#513925>l<#563E29>a<#5B422C>t<#60472F>e <#695035>E<#6E5438>g<#73593B>g
-    // {bold:1b, extra: [{text:"test",color:"#392216"}]}
-    // {"bold":true, extra: [{"color":"#392216","text":"C"},{"color":"#3E2719","text":"h"},{"color":"#432B1C","text":"o"},{"color":"#48301F","text":"c"},{"color":"#4C3422","text":"o"},{"color":"#513925","text":"l"},{"color":"#563E29","text":"a"},{"color":"#5B422C","text":"t"},{"color":"#60472F","text":"e "},{"color":"#695035","text":"E"},{"color":"#6E5438","text":"g"},{"color":"#73593B","text":"g"}],"text":""}
-    // {"bold":true,"color":"#392216","extra":[{"color":"#3E2719","extra":[{"color":"#432B1C","extra":[{"color":"#48301F","extra":[{"color":"#4C3422","extra":[{"color":"#513925","extra":[{"color":"#563E29","extra":[{"color":"#5B422C","extra":[{"color":"#60472F","extra":[{"color":"#695035","extra":[{"color":"#6E5438","extra":[{"color":"#73593B","text":"g"}],"text":"g"}],"text":"E"}],"text":"e "}],"text":"t"}],"text":"a"}],"text":"l"}],"text":"o"}],"text":"c"}],"text":"o"}],"text":"h"}],"text":"C"}
 }
